481c236a78a9d103953141d9def9c851b6a8af3a
TerZhang <terzhang@hotmail.com>
2019-09-10 19:49:29 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+port = 3000;
+
+io.on('connection', socket => {
+  console.log('User connected');
+  // turn the socket on at this connection
+  socket.on('chat message', msg => {
+    console.log(msg);
+    io.emit('chat message', msg); // emit msg to all client listening to this socket.io server
+  });
+});
+
+server.listen(port, () => {
+  console.log('server running on port ' + port);
+});
